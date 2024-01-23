44b4c3dc362f8d465a28bed911bdf08489e1157f
Anton Johansson <antojohansson1@gmail.com>
2024-01-23 16:23:11 +0100

    Basic beginnings...

--- RNN/parse_data.py
+++ RNN/parse_data.py
@@ -1,4 +1,27 @@
 #/usr/bin/env python3
-import panda as pd
+import pandas as pd
 
-print("hello")
+
+data_path = "../data/openb_pod_list_default.csv"
+
+
+def get_data():
+    df = pd.read_csv(data_path)
+    
+    df.drop(labels=["pod_phase",
+                    "num_gpu",
+                    "cpu_milli",
+                    "gpu_spec",
+                    "qos",
+                    "deletion_time",
+                    "memory_mib",
+                    "scheduled_time",
+                    "name",
+                    ],
+            axis=1,inplace=True)
+
+    return df
+
+
+
+
