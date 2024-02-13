2047897563e68dcab9428d9f80a1a45ac46b7821
Anton Johansson <antojohansson1@gmail.com>
2024-02-13 13:28:42 +0000

    generator now works

--- GAN/gan.py
+++ GAN/gan.py
@@ -18,13 +18,13 @@ class Generator(nn.Module):
     def dummy_function(self, dont=None, care=None):
         return torch.zeros(self.input_dim)
 
-    def __init__(self, input_dim=5, output_dim=1, layers=10):
+    def __init__(self, input_dim=5, output_dim=1, layers=2):
         super(Generator, self).__init__()
 
         self.input_dim = input_dim
         self.output_dim = output_dim
         self.layers = layers
-        self.operations = self.make_layers()
+        self.make_layers()
         self.lstm_zero()
         self.sig = nn.Sigmoid()
 
@@ -32,33 +32,30 @@ class Generator(nn.Module):
     def make_layers(self):
         operations = []
         self.L = nn.Linear(in_features=2, out_features=self.input_dim).double()
-
+        self.T = nn.ModuleList()
+        self.G = nn.ModuleList()
         for i in range(self.layers):
-            l = [ nn.LSTM(input_size=self.input_dim, hidden_size=self.input_dim).double(),
-                               nn.Linear(in_features=self.input_dim, out_features=self.output_dim).double()]
-            l = nn.ModuleList(l)
-            operations.append(l)
+            self.T.append(nn.LSTM(input_size=self.input_dim, hidden_size=self.input_dim).double())
+            self.G.append(nn.Linear(in_features=self.input_dim, out_features=self.output_dim).double())
+            
         self.last = nn.Linear(in_features=self.input_dim, out_features=self.output_dim).double()
-        return  nn.ModuleList(operations)
 
     def forward(self, z):
+        #print(z.size()[0])
         z = z.double()
         z = self.L(z)
+        count = 0
+        for t, g in zip(self.T, self.G):
+            res, self.internal_state[count] = t(z, self.internal_state[count])
+            mid_val = g(torch.randn(z.size()[0], self.input_dim).double())
+            z = res + mid_val
 
-        for i, e in enumerate(self.operations):
-            res, self.internal_state[i] = e[0](z, self.internal_state[i])
-            mid_val = e[1](torch.normal(mean=torch.tensor([0 for i in range(self.input_dim)]).double(),
-                                        std=torch.tensor([1 for i in range(self.input_dim)]).double()
-                                        )
-                           )
-            z = torch.add(res, mid_val)
         return self.sig(self.last(z))
 
     def lstm_zero(self):
         self.internal_state = []
         #first and last is not an lstm layer.
-        print(self.operations[0][0].all_weights[0][0][0][0])
-        for layer in self.operations:
+        for layer in self.T:
             self.internal_state.append([torch.zeros(1, self.input_dim).double(),
                                         torch.zeros(1, self.input_dim).double()])
 
