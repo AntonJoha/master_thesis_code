22b00d875f751cdc10ba13542b6e89e08e9c825b
Anton Johansson <antojohansson1@gmail.com>
2024-02-16 10:35:01 +0100

    Hello

--- RNN/parse_data.py
+++ RNN/parse_data.py
@@ -1,5 +1,7 @@
 #/usr/bin/env python3
 import pandas as pd
+import torch
+import numpy as np
 
 
 data_path = "../data/openb_pod_list_default.csv"
@@ -23,5 +25,52 @@ def get_data():
     return df
 
 
+def make_data(df):
+    
+    x_train, y_train = [], []
+    prev = -1
+    
+    m = df.max()[0]
+    print("Max value: ", m)
+    count = 0
+    for row in df.values:
+        x_train.append([count])
+        count += 1
+        y_train.append([row[0]/m])
+    return torch.tensor(x_train, dtype=torch.double)/count,torch.tensor(y_train, dtype=torch.double)
 
+
+def find_closest(val, options):
+    index = 0
+    min = 10000000
+    for i in range(len(options)):
+        if np.abs(val-options[i][0]) < min:
+            min = np.abs(val-options[i][0])
+            index = i
+    return index
+
+def get_modified_values(df):
+
+    dic = {}
+
+    for row in df.values:
+        if row[0] not in dic:
+            dic[row[0]] = 1
+        else:
+            dic[row[0]] += 1
+
+    dic_sort = sorted(dic.items(), key=lambda x:x[1])
+    dic_decreasing = list(reversed(dic_sort))
+
+    values = []
+    for i in range(10):
+        values.append(dic_decreasing[i])
+
+    val = []
+
+    for row in df.values:
+        closest = find_closest(row[0], values)
+        val.append([1.0 if i == closest else 0.0 for i in range(len(values))])
+
+    return torch.tensor(val)
 
---
+++ RNN/rnn.py
@@ -0,0 +1,47 @@
+import torch.nn as nn
+import torch
+
+class PredictTime(nn.Module):
+    def __init__(self, input_size=1,output_size=1,hidden_layers=1,hidden_size=1, device=None):
+
+        super().__init__()
+        
+
+        self.device=device
+
+        self.hidden_size = hidden_size
+        self.hidden_layers = hidden_layers
+        self.input_size = input_size
+        self.output_size = output_size
+        self.hidden_size = hidden_size
+
+        self.lstm = nn.LSTM(input_size=input_size, hidden_size=self.hidden_size, num_layers=self.hidden_layers)
+        self.linear = nn.Linear(self.hidden_size,self.output_size)
+
+        self.clean_state()
+
+        self.sig = nn.Sigmoid()
+        self.relu = nn.ReLU()
+        
+    def forward(self, x):
+        x, self.hidden = self.lstm(x,self.hidden)
+        x = self.relu(x)
+        x = self.linear(x)
+        return self.sig(x)
+    
+    def init_state(self):
+        self.h_0 = self.hidden[0].detach()
+        self.c_0 = self.hidden[1].detach()
+        self.hidden = (self.h_0, self.c_0)
+    
+    def clean_state(self):
+        self.h_0 = torch.zeros(self.hidden_layers,self.hidden_size, device=self.device)
+        self.c_0 = torch.zeros(self.hidden_layers,self.hidden_size, device=self.device)
+        self.hidden = (self.h_0, self.c_0)
+
+
+    def random_state(self):
+        self.h_0 = torch.randn(self.hidden_layers,self.hidden_size, device=self.device)
+        self.c_0 = torch.randn(self.hidden_layers,self.hidden_size, device=self.device)
+        self.hidden = (self.h_0, self.c_0)
+
